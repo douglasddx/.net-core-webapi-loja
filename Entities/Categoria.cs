ee9da8c7b8b6214ecb9788d1e78c7f645378e3f9
Douglas Damasceno <douglasddx@outlook.com>
2021-05-15 15:07:20 -0300

    aula_15_05_2021

--- Entities/Categoria.cs
+++ Entities/Categoria.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace loja.Entities
@@ -11,5 +12,7 @@ namespace loja.Entities
         [MinLength(3, ErrorMessage = "O tamanho mínino do atributo {0} é 3")]
         [MaxLength(50, ErrorMessage = "O tamanho máximo do atributo {0} é 50")]
         public string Nome { get; set; }
+
+        public IEnumerable<Produto> Produtos { get; set; }
     }
 }
